bd34dfb0541957f21c715dd1b20117b0827312b7
Omer Saleem <omer.saleem09@gmail.com>
2022-11-14 23:13:19 +0500

    Changed function name

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ from flask import request, url_for, render_template, redirect
 app = flask.Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
-def home():
+def index():
     return flask.render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
