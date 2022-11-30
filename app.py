301120f0f4c0d057fcaf3a02fa5f63d00552be0a
Omer Saleem <omer.saleem09@gmail.com>
2022-11-30 22:38:41 +0500

    Initialized basic map to work on Longitude and Lattitude Implemented geocoding API with 3D map Added route and mapbox access token for geocoding api

--- app.py
+++ app.py
@@ -4,10 +4,23 @@ from flask import request, url_for, render_template, redirect
 
 app = flask.Flask(__name__)
 
+mapbox_access_token="pk.eyJ1Ijoib21lcnNhbGVlbTA5IiwiYSI6ImNsYjNxMjhycTAwczEzeG9hYmprbjU4d3IifQ.RfTRwUxxxUKV4K5uWq3pRQ"
+
 @app.route('/',methods=['GET','POST'])
 def index():
     return flask.render_template('index.html')
 
+
+@app.route('/map',methods=['GET','POST'])
+def mapview():
+    return render_template('mapbox.html',
+    mapbox_access_token=mapbox_access_token)
+
+
+@app.route('/geo',methods=['GET','POST'])
+def geoencode():
+    return render_template('geocoding.html',
+    mapbox_access_token=mapbox_access_token)
 
 if __name__ == '__main__':
     app.run(debug=True)
